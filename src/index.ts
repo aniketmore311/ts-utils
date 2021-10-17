c2e90139744d97d68580325330e59092b59ea27e
aniket <aniketavinashmore343@gmail.com>
2021-10-17 23:08:48 +0530

    better docs

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,13 @@ import c from './colors'
   const logger = new Logger({
     logLevel: 'debug'
   })
+  // adding stratergies to logger
+  // you can define your own strategy which implements the IStrategy interface
+  // console stategy will give pretty output in the terminal
   logger.addStrategy(makeConsoleStrategy())
+  // file strategy will append json logs to a file
   logger.addStrategy(makeFileStrategy('app.json.log'))
+  // the extra object is printed on terminal and appended to the json log object in the file
   logger.info('hello world', { data: { hello: "world" } })
   logger.debug('hello world')
   logger.warning('hello world')
@@ -23,6 +28,7 @@ import c from './colors'
   console.log(`PORT - ${process.env.PORT}`)
 
   //using json database
+  // defining shape of database
   interface IDbData { users: { username: string, passoword: string }[] }
   const db = new JsonDB<IDbData>({
     path: "db.json",
@@ -30,9 +36,8 @@ import c from './colors'
       users: []
     }
   })
-  await db.read() //updates db.data property
+  await db.read() //updates db.data property from file
   db.data.users.push({ username: "aniket", passoword: "password123" })
   await db.write() // saves db.data to file
-  console.log(db.data)
-
+  console.log(db.data) // checkout the db.json file created on your system
 })()
