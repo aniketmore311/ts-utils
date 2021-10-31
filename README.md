6aafd5e46e14efc5db108e4eadac5de34984d9cc
aniket <aniketavinashmore343@gmail.com>
2021-10-31 20:51:04 +0530

    remove logger

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 ## minimal zero dependency typescript utilities for your next project
 
-- simple logger
 - simple json database
 - utility for colorized output
 - utility to load env variables from json file
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
-import { Logger, makeConsoleStrategy, makeFileStrategy } from "./Logger";
 import { JsonDB } from './JsonDB'
 import { loadenv } from './loadenv'
 import c from './colors'
 
 (async () => {
-  //using logger
-  const logger = new Logger()
-  // adding stratergies to logger
-  // you can define your own strategy which implements the IStrategy interface
-  // console stategy will give pretty output in the terminal
-  logger.addStrategy(makeConsoleStrategy())
-  // file strategy will append json logs to a file
-  // use options to add log level to strategy
-  logger.addStrategy(makeFileStrategy('app.json.log', {
-    logLevel: "error"
-  }))
-  // the extra object is printed on terminal and appended to the json log object in the file
-  logger.debug('hello world', { data: { hello: "world" } })
-  logger.info('hello world')
-  logger.warning('hello world')
-  logger.error('hello world')
 
   // using colors
   console.log(c.brightGreen('this is bright green'))
